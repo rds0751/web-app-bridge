d5e10ce04974d4e6062eb662256cf7d7fd3ed96a
dadwalsahil <sahild@leewayhertz.com>
2022-04-19 14:27:59 +0530

    bug fix

--- src/Modules/Common/drawer.js
+++ src/Modules/Common/drawer.js
@@ -4,15 +4,71 @@ import Toolbar from "@mui/material/Toolbar";
 import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
+import MuiListItem from "@material-ui/core/ListItem";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import MailIcon from "@mui/icons-material/Mail";
 import { Link } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import "./index.css";
+import { useEffect } from "react";
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     width: "100%",
+//     maxWidth: 360,
+//     backgroundColor: theme.palette.background.paper,
+//   },
+// }));
+
+// const ListItem = withStyles({
+//   root: {
+//     "&$selected": {
+//       color: "white",
+//       "& .MuiListItemIcon-root": {
+//         color: "white",
+//       },
+//     },
+//     "&$selected:hover": {
+//       color: "white",
+//       "& .MuiListItemIcon-root": {
+//         color: "white",
+//       },
+//     },
+//     "&:hover": {
+//       color: "white",
+//       "& .MuiListItemIcon-root": {
+//         color: "white",
+//       },
+//     },
+//   },
+//   selected: {},
+// })(MuiListItem);
 
 const drawerWidth = 200;
 export default function SideBar() {
+  // const classes = useStyles();
+
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+  const handleListItemClick = (index) => {
+    setSelectedIndex(index);
+  };
+  const [aboutIcon, setAboutIcon] = React.useState(
+    "/images/noun_Home_coloured.svg"
+  );
+  const [bridgeIcon, setBridgeIcon] = React.useState(
+    "/images/bridge_Colored.svg"
+  );
+  const [historyIcon, setHistoryIcon] = React.useState(
+    "/images/history_Coloured.svg"
+  );
+
+  // const changeSourceForIcons = (value) => {
+  //   if (value === "about") setAboutIcon("/images/noun_Home_white.svg");
+  // };
+  // const changeOriginalSourceForIcons = (value) => {
+  //   if (value === "about") setAboutIcon("/images/noun_Home_coloured.svg");
+  // };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Drawer
@@ -37,21 +93,43 @@ export default function SideBar() {
           </Toolbar>
         </div>
         <List>
-          <ListItem className="list-item" button component={Link} to="/about">
+          <ListItem
+            className="list-item"
+            button
+            selected={selectedIndex === 1}
+            onClick={(event) => handleListItemClick(1)}
+            component={Link}
+            to="/about"
+          >
             <ListItemIcon>
               <img
                 style={{ paddingLeft: "10px" }}
-                src="/images/noun_Home_3874019.svg"
+                src={
+                  selectedIndex == 1
+                    ? "/images/noun_Home_white.svg"
+                    : "/images/noun_Home_coloured.svg"
+                }
               ></img>
             </ListItemIcon>
             <ListItemText className="list-text">About</ListItemText>
           </ListItem>
 
-          <ListItem className="list-item" button component={Link} to="/bridge">
+          <ListItem
+            className="list-item"
+            button
+            selected={selectedIndex === 2}
+            onClick={(event) => handleListItemClick(2)}
+            component={Link}
+            to="/bridge"
+          >
             <ListItemIcon>
               <img
                 style={{ paddingLeft: "10px" }}
-                src="/images/BridgeWhite.svg"
+                src={
+                  selectedIndex == 2
+                    ? "/images/bridge_White.svg"
+                    : "/images/bridge_Colored.svg"
+                }
               ></img>
             </ListItemIcon>
             <ListItemText className="list-text">Bridge</ListItemText>
@@ -78,11 +156,22 @@ export default function SideBar() {
             <ListItemText className="list-text">Market</ListItemText>
           </ListItem> */}
 
-          <ListItem className="list-item" button component={Link} to="/history">
+          <ListItem
+            className="list-item"
+            button
+            selected={selectedIndex === 3}
+            onClick={(event) => handleListItemClick(3)}
+            component={Link}
+            to="/history"
+          >
             <ListItemIcon>
               <img
                 style={{ paddingLeft: "10px" }}
-                src="/images/history (1).svg"
+                src={
+                  selectedIndex == 3
+                    ? "/images/history_White.svg"
+                    : "/images/history_Coloured.svg"
+                }
               ></img>
             </ListItemIcon>
             <ListItemText className="list-text">History</ListItemText>
